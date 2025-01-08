95dc9161fbb188189b8f794f704a2252be4e5c4e
HimaBindu Nara <naarahimabindu@gmail.com>
2025-01-08 22:23:44 +0530

    Add files via upload

---
+++ Overview.py
@@ -0,0 +1,3 @@
+#1. Python is Dynamically Typed Programming Language.
+#2. In Python while declaring varaibles, we no need to mention Data Types.
+#3. DataTypes will be assigned by language based on value stored into the variables.
