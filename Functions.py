6facf561c4a33a135f397adf316fb7be6b6bdb15
HimaBindu Nara <naarahimabindu@gmail.com>
2025-01-09 20:53:31 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,27 @@
+# Without input and without return statement
+
+def add():
+    a=10
+    b=20
+    print("Addition is:",a+b)
+
+
+# With input and without return statement
+
+def sub(a,b):
+    print("Subtrcation is:",a-b)
+
+# Without input and with return statement:
+def mul():
+    return 10*20
+
+# with Inout and with return statements
+def div(a,b):
+    return a/b
+
+add()
+sub(100,50)
+print(mul())
+print(div(200,10))
+
+
